dc7a80d250f2adb87b0bfd6dcbf607afa30bd7a8
prabhath6 <prabhathkiran20@gmail.com>
2018-08-26 23:15:48 -0500

    Enable searching for title completed

--- src/emoji_search_clojurescript/display_emojis.cljs
+++ src/emoji_search_clojurescript/display_emojis.cljs
@@ -7,8 +7,11 @@
     [:tbody
      (for [emoji (take 25  @st/filtered-emojis)]
        ^{:key (get emoji :title)}
+       ;;TODO: Add on click event to copy the content to clipboard.
        [:tr [:td
              [:div.columns [:div.column
                             [:figure {:class "image is-32x32"}
-                             [:img {:src (str "//cdn.jsdelivr.net/emojione/assets/png/" (.toString (.codePointAt (get emoji :symbol) 0) 16) ".png")}]]]
+                             [:img
+                              {:src (str "//cdn.jsdelivr.net/emojione/assets/png/"
+                                         (.toString (.codePointAt (get emoji :symbol) 0) 16) ".png")}]]]
               [:div.column (get emoji :title)]]]])]]])
--- src/emoji_search_clojurescript/search_handler.cljs
+++ src/emoji_search_clojurescript/search_handler.cljs
@@ -1,11 +1,16 @@
 (ns emoji-search-clojurescript.search-handler
   (:require [emoji-search-clojurescript.state :as st]))
 
-;;TODO: Update this filter to look into title too.
-(defn filter-helper [filter-term emoji]
-  (if (or (= filter-term "") (= filter-term " "))
-    emoji
+(defn keyword-search-helper [filter-term emoji]
+  (if (not (or (= filter-term "") (= filter-term " ")))
     (.includes (.toLowerCase (get emoji :keywords)) filter-term)))
+
+(defn search-title-and-keywords [filter-term emoji]
+  (let [keyword-results (keyword-search-helper filter-term emoji)
+        title-results (.includes (.toLowerCase (get emoji :title)) filter-term)]
+    (if title-results
+      title-results
+      keyword-results)))
 
 (defn search-handler []
   [:input.input.is-medium.is-success
@@ -16,4 +21,4 @@
                  (let [filter-term (reset! st/search-term (.. e -target -value))
                        emoji-state @st/emojis]
                    (reset! st/filtered-emojis
-                           (filter (partial filter-helper filter-term) emoji-state))))}])
+                           (filter (partial search-title-and-keywords filter-term) emoji-state))))}])
