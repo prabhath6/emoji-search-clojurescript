93934071f22f8b8777ec5d5e2417c5e61978fc78
prabhath6 <prabhathkiran20@gmail.com>
2018-08-28 00:21:11 -0500

    Completed clipboard feature

--- src/emoji_search_clojurescript/display_emojis.cljs
+++ src/emoji_search_clojurescript/display_emojis.cljs
@@ -11,12 +11,18 @@
              title-id (str "emoji-title-" title)]
         ^{:key title}
         [:tr [:td
-              [:a
-               ;;{:on-click (fn [e]
-               ;;                 (prn (. js/document (.getElementById js/document (str "emoji-title-" (get emoji :title))))))}
-                [:div.columns [:div.column
+              [:div.columns [:div.column
+                             [:a
+                              {:on-click (fn []
+                                           (let [textInput (js/document.createElement "textarea")] ; create
+                                             (set! (.-style textInput) #js {:display "none"})
+                                             (set! (.-innerHTML textInput) (get emoji :symbol))
+                                             (js/document.body.appendChild textInput) ; add to body
+                                             (.select textInput)
+                                             (.execCommand js/document "copy")
+                                             (js/window.document.body.removeChild textInput)))}
                               [:figure {:class "image is-32x32"}
                               [:img
                                 {:src (str "//cdn.jsdelivr.net/emojione/assets/png/"
-                                          (.toString (.codePointAt symbol 0) 16) ".png")}]]]
-                 [:div.column [:p {:id title-id } title]]]]]]))]]])
+                                          (.toString (.codePointAt symbol 0) 16) ".png")}]]]]
+                 [:div.column [:p {:id title-id } title]]]]]))]]])
