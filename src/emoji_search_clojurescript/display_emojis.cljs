954bcdee1ac5e45479657619ae5b0212c315214e
prabhath6 <prabhathkiran20@gmail.com>
2018-08-28 23:11:23 -0500

    Change the column order

--- src/emoji_search_clojurescript/display_emojis.cljs
+++ src/emoji_search_clojurescript/display_emojis.cljs
@@ -11,18 +11,19 @@
              title-id (str "emoji-title-" title)]
          ^{:key title}
          [:tr [:td
-               [:div.columns [:div.column
-                              [:a
-                               {:on-click (fn []
-                                            (let [textInput (js/document.createElement "textarea")] ; create
-                                              (set! (.-style textInput) #js {:display "none"})
-                                              (set! (.-innerHTML textInput) (get emoji :symbol))
-                                              (js/document.body.appendChild textInput) ; add to body
-                                              (.select textInput)
-                                              (.execCommand js/document "copy")
-                                              (js/window.document.body.removeChild textInput)))}
-                               [:figure {:class "image is-32x32"}
-                                [:img
-                                 {:src (str "//cdn.jsdelivr.net/emojione/assets/png/"
-                                            (.toString (.codePointAt symbol 0) 16) ".png")}]]]]
-                [:div.column [:p {:id title-id} title]]]]]))]]])
+               [:div.columns
+                [:div.column [:p {:id title-id} title]]
+                [:div.column
+                 [:a
+                  {:on-click (fn []
+                               (let [textInput (js/document.createElement "textarea")] ; create
+                                 (set! (.-style textInput) #js {:display "none"})
+                                 (set! (.-innerHTML textInput) (get emoji :symbol))
+                                 (js/document.body.appendChild textInput) ; add to body
+                                 (.select textInput)
+                                 (.execCommand js/document "copy")
+                                 (js/window.document.body.removeChild textInput)))}
+                  [:figure {:class "image is-32x32"}
+                   [:img
+                      {:src (str "//cdn.jsdelivr.net/emojione/assets/png/"
+                                (.toString (.codePointAt symbol 0) 16) ".png")}]]]]]]]))]]])
